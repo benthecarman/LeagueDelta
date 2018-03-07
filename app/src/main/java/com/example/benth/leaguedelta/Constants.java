68253bfe41bfb0f92519e9980a296789c9fda03a
Ben Carman <benthecarman@live.com>
2018-03-06 18:01:21 -0600

    Code Cleanup

--- app/src/main/java/com/example/benth/leaguedelta/Constants.java
+++ app/src/main/java/com/example/benth/leaguedelta/Constants.java
@@ -4,7 +4,7 @@ import net.rithms.riot.constant.Platform;
 
 class Constants {
 
-    final static String API_KEY = "RGAPI-b5b32557-91d7-4192-935d-89b656b523d7";
+    final static String API_KEY = "RGAPI-b1b1a2bc-c99b-4703-a005-b3412b0f19ad";
     static String LOL_VERSION;
     static Platform platform;
 }
